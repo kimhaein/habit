5e6202719c7248de6f02525a30b26c5c3a10d04f
haein <kendy93@sbcn.co.kr>
2018-12-24 16:48:01 +0900

    [진행] redux 설정 중

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,33 @@
 import React, { Component,Fragment } from 'react';
-import HomeScreen from './HomeScreen';
+// Nav
+import TopNav from '../nav/TopNav';
+import SideNav from '../nav/SideNav';
+// screen
+import DetailScreen from '../app/DetailScreen';
+import HomeScreen from '../app/HomeScreen';
 
 class App extends Component {
+  state = {
+    isDetail:true,
+    screen:'',
+  }
+  __changeScreen = () => {
+    // this.setState({
+    
+    // })
+  }
+  __renderScreen = () => {
+    switch (this.state.screen) {
+      case 'Detail': return <DetailScreen/>
+      default: return <HomeScreen />
+    }
+  }
   render() {
     return (
       <Fragment>
-          <HomeScreen/>
+        <TopNav/>
+        <SideNav/>
+        {this.__renderScreen()}
       </Fragment>
     );
   }
--- src/components/app/HomeScreen.js
+++ src/components/app/HomeScreen.js
@@ -1,33 +1,18 @@
-import React, { Component,Fragment } from 'react';
-// screen
-import TopNav from '../nav/TopNav';
-import SideNav from '../nav/SideNav';
+import React, { Component } from 'react';
+// common
 import CardList from '../common/CardList';
 import MainSlider from '../common/MainSlider';
 
 class HomeScreen extends Component {
-  state = {
-    isLogin:true,
-  }
-
-  __renderMain = (isLogin) => {
-    if(isLogin){
-      return  <TopNav/>
-    }
-  }
-
   render() {
     return (
-      <Fragment>
-        <TopNav/>
-        <SideNav/>
-        <div className="main_wrap">
-          <MainSlider/>
-          <div className="card_wrap">
-            <CardList/>
-          </div>
+      <div className="main_wrap">
+        <MainSlider/>
+        <div className="card_wrap">
+          <CardList/>
+          <CardList/>
         </div>
-      </Fragment>
+      </div>
     );
   }
 }
